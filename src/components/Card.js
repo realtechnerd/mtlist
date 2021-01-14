c32ab50fac396c4a5787c4d32a678619f4e6c552
realtechnerd <send.nithil@gmail.com>
2021-01-14 10:16:47 -0500

    Card text fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -38,15 +38,15 @@ export default function Card(props) {
                     </ReactReadMoreReadLess>
                     <br/>
                     <div className="overflow-hidden mb-2">
-                        <button className="md:float-left border-none bg-white" {...(props.clients && { onClick: handleOpen })} style={{cursor: !props.clients && "text", userSelect: !props.clients && "text"}}><h5 className="mb-0 mt-2 text-black"><b>Online Clients:</b> {props.clients}</h5></button>
-                        <h5 className="mb-0 mt-2.5 text-black md:float-right"><b>Max Clients:</b> {props.max_clients}</h5>
+                        <button className="md:float-left border-none bg-white ml-0 p-0" {...(props.clients && { onClick: handleOpen })} style={{cursor: !props.clients && "text", userSelect: !props.clients && "text"}}><h5 className="mb-0 mt-2 text-black"><b>Online Clients:</b> {props.clients}</h5></button>
+                        <h5 className="mb-0 mt-2.5 text-black md:float-right p-0"><b>Max Clients:</b> {props.max_clients}</h5>
                     </div>
                     <a href={props.url} target="_blank" className="mb-0 text-black" style={{display: !props.url && "none"}}><h6><span>{props.url}</span></h6></a>  
                     <div className="flex overflow-hidden">
-                        <Tippy content={<p style={{fontFamily: "Inter"}} className="mb-0">PVP Enabled</p>}>
+                        <Tippy content={<p style={{fontFamily: "Inter"}} className="mb-0 float-left">PVP Enabled</p>}>
                             <img src={Pvp} style={{height: "28px", display: !props.pvp && "none"}} className="mr-1.5"/>
                         </Tippy>   
-                        <Tippy content={<p style={{fontFamily: "Inter"}} className="mb-0">Creative Mode On</p>}>
+                        <Tippy content={<p style={{fontFamily: "Inter"}} className="mb-0 float-left">Creative Mode On</p>}>
                             <img src={Creative} style={{height: "28px", display: !props.creative && "none"}}/>
                         </Tippy>    
                     </div>
